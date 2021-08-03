a036c3c4a8825e8f95b316de2c37d6650b27a4ae
deyvisongdias <deyvisondias@hotmail.com>
2021-08-03 18:01:36 -0300

    l

--- FinancialSystem/financialSystem/urls.py
+++ FinancialSystem/financialSystem/urls.py
@@ -1,4 +1,4 @@
-"""SistemaDeGerenciamento URL Configuration
+"""financialSystem URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
